5427f09c0c32105286200010fb21de2737369ac7
dylan909 <73878448+dylan909@users.noreply.github.com>
2023-03-08 16:25:24 +0000

    added Routes + middleware + different file structure

--- index.js
+++ index.js
@@ -1,70 +1,31 @@
 const express = require("express");
-
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const {User, Song} =  require("./models/index");
 const {sequelize} = require("sequelize");
+const { setUser } = require("./middleware");
+const {musicRouter, userRouter} = require('./routers')
 
 
-// Get the secret from the .env file
 const JWT_SECRET = process.env.JWT_SECRET;
-
-// Function to hash the paswords
-const hashPassword = async (password, saltCount) => {
-    const hash = await bcrypt.hash(password, saltCount);
-    return hash;
-}
-
-const register = async (user, password) => {
-    // This creates the table, dropping it first if it already existed
-    await db.sync();
-    try {
-        const hashedPw = await hashPassword(password, 9);
-        let {name, id, username} = await User.create({name: name, username: username, password: hashedPw}); 
-        // Create JWT token here
-        const token = jwt.sign({id, username}, JWT_SECRET);
-        return {message: "Thanks for registering! Here is your token", token};
-    } catch(err) {
-        console.error(err);
-    }
-}
-
-const login = async (username, password) => {
-    try {
-        const [foundUser] = await User.findAll({where: {username}});
-        if(!foundUser) {
-            return 'Failed';
-        }
-        const isMatch = await bcrypt.compare(password, foundUser.password);
-        if(isMatch) {
-            // Create JWT token here
-          const token = jwt.sign(username, JWT_SECRET);
-            return {message: "Welcome back! Here is your token", token};
-        } else {
-            return 'Failed';
-        }
-    } catch(err) {
-        console.error(err);
-    }
-}
-
-
-// register(username, plainTextPW)
-// .then((result) => {
-//     console.log(result);
-// });
-
-// login(username, plainTextPW)
-// .then((result) => {
-//     console.log(result)
-// });
-
 const SALT_COUNT = 10;
 const app = express();
+
+
+
+
+//MIDDLEWEAR -----------------------------------------------------------
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(setUser)
 
+
+
+
+//ROUTES -------------------------------------------------------------
 
 app.get("/", (req, res) => {
   res.send(
@@ -72,96 +33,8 @@ app.get("/", (req, res) => {
   );
 });
 
-app.get("/user/:id", async (req, res) => {
-  const { id } = req.params;
+app.use('/user', userRouter)
+app.use("/music", musicRouter)
 
-  try {
-  const user = await User.findByPk(id);
-   res.send(user);
-  } catch (error) {
-    console.log(error);
-    res.send(error).sendStatus(403)
-  }
- 
-});
-
-
-app.get("/Artistmusic/:id", async (req, res) => {
-  const {id} = req.params
-  const songs = await Song.findAll({where:{user_id: id}});
-  res.send(songs);
-});
-
-app.get("/music", async (req, res) => {
-  const songs = await Song.findAll();
-  res.send(songs);
-});
-
-app.get("/music/:id", async (req, res) => {
-  const { id } = req.params; 
-  if (id != req.user.id) {
-    res.sendStatus(403);
-  }
-  const song = await Song.findByPk(id); 
-  res.send(song);
-});
-
-app.post("/music", async (req, res) => { 
-  const song = await Song.create(req.body, {});
-  res.send(song);
-});
-
-app.post("/register", async (req, res, next) => {
-  try {
-    const { name,username, password } = req.body;
-    const hashed = await bcrypt.hash(password, SALT_COUNT);
-    await User.create({ 
-      name: name,
-      username: username,
-       password: hashed 
-      });
-    res.send(`User "${username}" was successfully created.`);
-  } catch (error) {
-    console.error(error); 
-    next(error);
-  }
-});
-
-app.post("/login", async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    const [foundUser] = await User.findOne({ where: { username } });
-    // hash compare
-    if (!foundUser) {
-      res.send("User not found");
-      return;
-    }
-    // hash compare#
-    const isMatch = await bcrypt.compare(password, foundUser.password);
-    if (!isMatch) {
-      res.status(401).send("Username or password is incorrect.");
-      return;
-    }
-    res.send(`successfully logged in user: ${username}`);
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
-
-app.put("/music/:id", async (req, res) => {
-  const { id } = req.params;
-  const song = await Song.findByPk(id);
-  const song1 = await song.update(req.body, { where: { id: id } });
-  res.send(song1);
-});
-
-app.delete("/music/:id", async (req, res) => {
-  const { id } = req.params;
-  await Song.destroy({
-    where: { id },
-  });
-  res.send(`Deleted song ${id}`);
-});
 
 module.exports = app;
---
+++ routers/userRouter.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const router = express.Router()
+const {Song, User} = require('../models')
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const SALT_COUNT = 10;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.get(":id", async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+     const user = await User.findByPk(id);
+     res.send(user);
+    } catch (error) {
+      console.log(error);
+      res.send(error).sendStatus(403)
+    }
+   
+  });
+  
+  
+router.post("/register", async (req, res, next) => {
+    try {
+      const { name,username, password, isStaff, isSelena } = req.body;
+      const hashed = await bcrypt.hash(password, SALT_COUNT);
+      const {id, Name} = await User.create({ 
+        name: name,
+        username: username,
+        password: hashed,
+        isStaff: isStaff,
+        isSelena: isSelena
+      });
+      const token = jwt.sign({id, Name, isStaff, isSelena}, JWT_SECRET)
+      res.send({message: "You are logged in!", token})
+    } catch (error) {
+      console.error(error); 
+      next(error);
+    }
+  });
+  
+router.post("/login", async (req, res, next) => {
+    try {
+      const {username, password} = req.body
+      const foundUser = await User.findOne({where: {username}});
+      console.log("Found User", foundUser)
+          if(!foundUser) {
+            console.log('yup')
+              res.sendStatus(401);
+          }
+          console.log(foundUser.password)
+          const isMatch = await bcrypt.compare(password, foundUser.password);
+          if(isMatch) {
+              const {id, name, isStaff, isSelena} = foundUser
+              const token = jwt.sign({id, name, isStaff, isSelena}, JWT_SECRET);
+              res.send({message: "Welcome back! Here is your token", token})
+          } else {
+            console.log('yuppp')
+              res.sendStatus(401)
+          }
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  });
+
+
+router.delete('/delete', async (req,res) => {
+  console.log("User Objext in /User/delete", req.user)
+  if (req.user.isSelena == true){
+    const {id} = req.body;
+    const deletedUser = await User.findByPk(id)
+    User.destroy({
+      where: {
+        id: id
+      }
+    })
+    res.send({message: "User Deleted", deletedUser})
+    return
+  }
+  res.send(401)
+
+})
+
+  module.exports = router 
